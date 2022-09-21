39c6201c6c960eb2f6c64fd65f1d4fd6e054dc0a
tmnds <trmendes0@gmail.com>
2022-09-21 08:23:09 -0300

    import do modulo crypto_hash e funcao que realiza conversao

--- backend/utils/hex_to_binary.py
+++ backend/utils/hex_to_binary.py
@@ -1,3 +1,5 @@
+from backend.utils.crypto_hash import crypto_hash
+
 HEX_TO_BINARY_COVERSION_TABLE = {
     '0': '0000',
     '1': '0001',
@@ -36,6 +38,8 @@ def main():
     original_number = int(binary_number, 2)
     print(f'original_number: {original_number}')
 
+    crypto_hash_to_binary = hex_to_binary(crypto_hash('foo-data'))
+    print(f'crypto_hash_to_binary: {crypto_hash_to_binary}')
 
 if __name__ == '__main__':
     # hex_to_binary('55a9f4f8994b1bbf2058ea38c8efb6c459000814d5f39c087002571639e6230e')
