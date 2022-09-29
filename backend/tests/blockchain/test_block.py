df1006797a97f6d07d59f009f0dd467df5a21c45
tmnds <trmendes0@gmail.com>
2022-09-29 08:34:30 -0300

    tests is_valid_block

--- backend/tests/blockchain/test_block.py
+++ backend/tests/blockchain/test_block.py
@@ -75,3 +75,22 @@ def test_is_valid_block_bad_last_hash(last_block, block):
     with pytest.raises(Exception, match='last_hash must be correct'):
         Block.is_valid_block(last_block, block)
 
+def test_is_valid_bad_proof_of_work(last_block, block):
+    block.hash = 'fff'
+
+    with pytest.raises(Exception, match='proof of requirement was not met'):
+        Block.is_valid_block(last_block, block)
+
+def test_is_valid_block_jumped_difficulty(last_block, block):
+    jumped_difficulty = 10
+    block.difficulty = jumped_difficulty
+    block.hash = f'{"0" * jumped_difficulty}'
+
+    with pytest.raises(Exception, match='difficulty must only adjust by 1'):
+        Block.is_valid_block(last_block, block)
+
+def test_is_valid_block_bad_block_hash(last_block, block):
+    block.hash = '0000000000abff843a001'
+
+    with pytest.raises(Exception, match='block hash must be correct'):
+        Block.is_valid_block(last_block, block)
