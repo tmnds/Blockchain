c4799d2526a2470ea80e84088ba68290e089b621
tmnds <trmendes0@gmail.com>
2022-10-21 08:26:12 -0300

    wallet module

--- backend/wallet/wallet.py
+++ backend/wallet/wallet.py
@@ -1,7 +1,8 @@
-from ctypes import addressof
-
-
 import uuid 
+
+from backend.config import STARTING_BALANCE
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import ec
 
 
 class Wallet:
@@ -13,3 +14,14 @@ class Wallet:
 
     def __init__(self):
         self.address = str(uuid.uuid4())[0:8]
+        self.private_key = ec.generate_private_key(
+            ec.SECP256K1(),
+            default_backend()
+        )
+        self.public_key = self.private_key.public_key()
+
+def main():
+    wallet = Wallet()
+    print(f'wallet_address: {wallet.address}')
+    print(f'private_key: {wallet.private_key}')
+    print(f'public_key: {wallet.public_key}')
