529f211bf088e07ae07dc7dcfb23184f96863715
tmnds <trmendes0@gmail.com>
2022-10-21 08:54:12 -0300

    inclusao de metodo sign em wallet

--- backend/wallet/wallet.py
+++ backend/wallet/wallet.py
@@ -1,8 +1,10 @@
+import json
 import uuid 
 
 from backend.config import STARTING_BALANCE
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.hazmat.primitives import hashes
 
 
 class Wallet:
@@ -14,14 +16,30 @@ class Wallet:
 
     def __init__(self):
         self.address = str(uuid.uuid4())[0:8]
+        self.balance = STARTING_BALANCE
         self.private_key = ec.generate_private_key(
             ec.SECP256K1(),
             default_backend()
         )
         self.public_key = self.private_key.public_key()
 
+    def sign(self, data):
+        """
+        Generate a signature based on the data using the local private key.
+        """
+        return self.private_key.sign(
+            json.dumps(data).encode('utf-8'), # TypeError: data must be bytes-like
+            ec.ECDSA(hashes.SHA256())
+         )
+
+
 def main():
     wallet = Wallet()
-    print(f'wallet_address: {wallet.address}')
-    print(f'private_key: {wallet.private_key}')
-    print(f'public_key: {wallet.public_key}')
+    print(f'wallet.__dict__: {wallet.__dict__}')
+
+    data = {'foo': 'bar'}
+    signature = wallet.sign(data)
+    print(f'signature: {signature}')
+
+if __name__ == '__main__':
+    main()
