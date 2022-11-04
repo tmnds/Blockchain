83406e0f40254ff30c2362e7be7843b6c55efc40
tmnds <trmendes0@gmail.com>
2022-11-04 09:02:15 -0300

    update tests code

--- backend/tests/wallet/test_transaction.py
+++ backend/tests/wallet/test_transaction.py
@@ -55,4 +55,18 @@ def test_transaction_update():
         transaction.input['signature']
     )
 
+    to_first_again_amount = 25
+
+    transaction.update(sender_wallet, first_recipient, to_first_again_amount)
+
+    assert transaction.output[first_recipient] == first_amount + to_first_again_amount
+    assert transaction.output[sender_wallet.address] == \
+        sender_wallet.balance - first_amount - next_amount - to_first_again_amount
+    assert Wallet.verify(
+        transaction.input['public_key'],
+        transaction.output, 
+        transaction.input['signature']
+    )
+
+
 
