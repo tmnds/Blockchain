ce2f04a17a0b84846461f69df1e3580d9d73a0e9
tmnds <trmendes0@gmail.com>
2022-12-15 10:29:22 -0300

    update frontend code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,36 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Joke from './Joke';
 
 function App() {
+  const [userQuery, setUserQuery] = useState('');
+  
+  const updateUserQuery = event => {
+    console.log('userQuery', userQuery);
+
+    setUserQuery(event.target.value);
+  }
+
+  const searchQuery = () => {
+    window.open(`https://google.com/search?q=${userQuery}`);
+  }
+
+  const handleKeyPress = event => {
+    if (event.key === 'Enter') {
+      searchQuery();
+    }
+  }
   return (
     <div className="App">
       <header className="App-header">
+      <input value={userQuery} onChange={updateUserQuery} onKeyUpCapture={handleKeyPress}/>
+      <button onClick={searchQuery}>Search</button>
+      <div>{userQuery}</div>
+        <hr />
+        <Joke />
         <img src={logo} className="App-logo" alt="logo" />
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -20,7 +45,10 @@ function App() {
         >
           Learn React
         </a>
+       
+      
       </header>
+
     </div>
   );
 }
