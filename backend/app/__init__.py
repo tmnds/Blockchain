6e9481c7fbb510fcc6dbfce0953b94a79b1132ce
tmnds <trmendes0@gmail.com>
2022-12-21 08:49:50 -0300

    update code

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -3,6 +3,7 @@ import random
 import requests 
 
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 
 from backend.blockchain.blockchain import Blockchain
 from backend.wallet.wallet import Wallet
@@ -10,7 +11,8 @@ from backend.wallet.transaction import Transaction
 from backend.wallet.transaction_pool import TransactionPool
 from backend.pubsub import PubSub
 
-app = Flask(__name__) 
+app = Flask(__name__)
+CORS(app, resources={ r'/*': {'origins': 'http://localhost:3000' } })
 blockchain = Blockchain()
 wallet = Wallet(blockchain)
 transaction_pool = TransactionPool()
